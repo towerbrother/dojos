39e11d7f28c8115fdb9f83f9110cb891fc00c6ed
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-15 14:52:08 +0000

    leapYears completed

---
+++ dojos/leapYears.js
@@ -0,0 +1,7 @@
+function isLeap(year) {
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0 && year % 400 !== 0) return false;
+  if (year % 4 === 0 && year % 100 !== 0) return true;
+}
+
+console.log(isLeap(2100));
