4129a8fc345b644ab6667278ef1ebab7ebe7ad31
Giorgio Torre <giorgio.torre@stellenanzeigen.de>
2021-01-12 09:50:47 +0100

    Potter code refactoring, started new solution

--- Potter/Potter/Program.cs
+++ Potter/Potter/Program.cs
@@ -29,6 +29,62 @@ namespace Potter
         public int Price { get; set; }
         public int Copies { get; set; }
     }
+
+    class Discount
+    {
+        public double Discount1 { get; }
+        public double Discount2 { get; }
+        public double Discount3 { get; }
+        public double Discount4 { get; }
+        public double Discount5 { get; }
+
+        public Discount()
+        {
+            Discount1 = 0.00;
+            Discount2 = 0.05;
+            Discount3 = 0.10;
+            Discount4 = 0.20;
+            Discount5 = 0.25;
+        }
+    }
+
+    class Books
+    {
+        public List<Book> Set { get; }
+
+        public Books(Book book1, Book book2, Book book3, Book book4, Book book5)
+        {
+            Set.Add(book1);
+            Set.Add(book2);
+            Set.Add(book3);
+            Set.Add(book4);
+            Set.Add(book5);
+        }
+
+        public int TotalCopies()
+        {
+            int totalCopies = 0;
+            foreach (Book book in Set)
+            {
+                totalCopies += book.Copies;
+            }
+
+            return totalCopies;
+        }
+
+        public int Max()
+        {
+            int max = 0;
+            foreach (Book book in Set)
+            {
+                if (book.Copies > max)
+                    max = book.Copies;
+            }
+
+            return max;
+        }
+    }
+
     class Program
     {
 
@@ -36,8 +92,9 @@ namespace Potter
         {
             Console.WriteLine("Start");
 
+            //var r = new Random();
+            //var copies = new int[5] { r.Next(6), r.Next(6), r.Next(6), r.Next(6), r.Next(6) };
             var copies = new int[5];
-
             // user to input
             for(int i = 1; i <= 5; i++)
             {
@@ -46,22 +103,19 @@ namespace Potter
                 copies[i - 1] = int.Parse(s);
             }
 
-            // initialise list
-            var books = new List<Book> { new Book { Number = 1, Price = 8, Copies = copies[0] }, new Book { Number = 2, Price = 8, Copies = copies[1] }, new Book { Number = 3, Price = 8, Copies = copies[2] }, new Book { Number = 4, Price = 8, Copies = copies[3] }, new Book { Number = 5, Price = 8, Copies = copies[4] } };
+            var book1 = new Book { Number = 1, Price = 8, Copies = copies[0] };
+            var book2 = new Book { Number = 2, Price = 8, Copies = copies[1] };
+            var book3 = new Book { Number = 3, Price = 8, Copies = copies[2] };
+            var book4 = new Book { Number = 4, Price = 8, Copies = copies[3] };
+            var book5 = new Book { Number = 5, Price = 8, Copies = copies[4] };
 
-            double price = 0;
-            double discount2 = 0.05;
-            double discount3 = 0.1;
-            double discount4 = 0.2;
-            double discount5 = 0.25;
+            // initialise
+            var price = 0.00;
+            var books = new Books(book1, book2, book3, book4, book5);
+            var discounts = new Discount();
+            var totalCopies = books.TotalCopies();
+            var max = books.Max();
 
-            // calculate total copies
-            int totalCopies = 0;
-            foreach (Book book in books)
-            {
-                totalCopies += book.Copies;
-            }
-             
             // case 0
             if (totalCopies == 0)
             {
@@ -71,35 +125,26 @@ namespace Potter
             else if (totalCopies == 1)
             {
                 Console.WriteLine("Total price: 8 Euros");
-
             }
             // case 2+
             else
             {
-                // max value will tell us number of columns
-                int max = 0;
-                foreach (Book book in books)
-                {
-                    if (book.Copies > max)
-                        max = book.Copies;
-                }
-
-                // if max is 1 than there is only one column
+                // only one column
                 if (max == 1)
                 {
                     switch (totalCopies)
                     {
                         case 3:
-                            price += (8 * 3 * (1 - discount3));
+                            price += (books.Set[2].Price * 3 * (1 - discounts.Discount3));
                             break;
                         case 4:
-                            price += (8 * 4 * (1 - discount4));
+                            price += (books.Set[3].Price * 4 * (1 - discounts.Discount4));
                             break;
                         case 5:
-                            price += (8 * 5 * (1 - discount5));
+                            price += (books.Set[4].Price * 5 * (1 - discounts.Discount5));
                             break;
                         default:
-                            price += (8 * 2 * (1 - discount2));
+                            price += (books.Set[1].Price * 2 * (1 - discounts.Discount2));
                             break;
                     }
                 } 
@@ -107,52 +152,34 @@ namespace Potter
                 else
                 {
                     // populate the array with 1s and/or 0s.
-                    var matrix = new int[books.Count, max];
-                    for (int i = 0; i < books.Count; i++)
+                    var matrix = new int[books.Set.Count, max];
+                    for (int i = 0; i < books.Set.Count; i++)
                     {
-                        int numCopies = books[i].Copies;
-                        for (int j = 0; j < max; j++)
+                        // books.Set[i].Copies == max --> put 1 everywhere
+                        if (books.Set[i].Copies == max)
                         {
-                            if (numCopies > 0)
+                            for (int j = 0; j < max; j++)
                             {
                                 matrix[i, j] = 1;
-                                numCopies--;
-                            }
-                            else
-                            {
-                                matrix[i, j] = 0;
                             }
                         }
-                    }
-
-                    var sums = new int[max];
-                    for (int j = 0; j < max; j++)
-                    {
-                        int columnSum = 0;
-                        for (int i = 0; i < books.Count; i++)
+                        else
                         {
-                            columnSum += matrix[i, j];
+                            // books.Set[i].Copies < max --> check price dinamically --> keep MIN price
+                            var numCopies = books.Set[i].Copies;
+                            for (int j = 0; j < max; j++)
+                            {
+                                if (numCopies > 0)
+                                {
+                                    matrix[i, j] = 1;
+                                    numCopies--;
+                                }
+                                else
+                                {
+                                    matrix[i, j] = 0;
+                                }
+                            }
                         }
-
-
-                        //switch (columnSum)
-                        //{
-                        //    case 2:
-                        //        price += (8 * 2 * (1 - discount2));
-                        //        break;
-                        //    case 3:
-                        //        price += (8 * 3 * (1 - discount3));
-                        //        break;
-                        //    case 4:
-                        //        price += (8 * 4 * (1 - discount4));
-                        //        break;
-                        //    case 5:
-                        //        price += (8 * 5 * (1 - discount5));
-                        //        break;
-                        //    default:
-                        //        price += 8;
-                        //        break;
-                        //}
                     }
                 }
                 Console.WriteLine("Total price: {0} Euros", price);
